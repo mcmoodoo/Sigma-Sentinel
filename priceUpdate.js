e9b8120fb69be0b742e30862d0d7864f2a141348
mcmoodoo <rashid@mcmoodoo.com>
2025-10-12 12:38:10 -0500

    hitting hermes

--- priceUpdate.js
+++
@@ -1,8 +0,0 @@
-const connection = new HermesClient("http://hermes.pyth.network", {});
-
-const priceIds = [
-   "0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace" // ETH/USD
-];
-
-const priceUpdates = await connection.getLatestPriceUpdates(priceIds);
-console.log(priceUpdates);
